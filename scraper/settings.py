a533343458689c30cde405ed232f0a41226f5ba5
Ugochukwu Nwachukwu <danielnwachukwu5738@gmail.com>
2023-03-10 21:21:49 +0100

    second commit

--- scraper/settings.py
+++ scraper/settings.py
@@ -1,5 +1,8 @@
 from pathlib import Path
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -8,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure--fsdqg)z83&(5el$qf*xi3gnhen@x^vosm%6zieo-@$f7*cv-#"
+SECRET_KEY = os.getenv("SETTINGS_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
